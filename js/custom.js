d281bec3132e05d8a14995aa5d5d290e7ae06907
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2022-08-01 00:35:53 +0530

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+
+function homeScroll() {
+
+  $('html, body').animate({
+
+    scrollTop: $(".home-index").offset().top - 120
+  }, 1000);
+}
+
+function serviceScroll() {
+
+  $('html, body').animate({
+
+    scrollTop: $(".service-index").offset().top - 120
+  }, 1000);
+}
+
+function aboutScroll() {
+
+  $('html, body').animate({
+
+    scrollTop: $(".about-index").offset().top - 120
+  }, 1000);
+}
+
+function contactScroll() {
+
+  $('html, body').animate({
+
+    scrollTop: $(".contact-index").offset().top - 120
+  }, 1000);
+}
